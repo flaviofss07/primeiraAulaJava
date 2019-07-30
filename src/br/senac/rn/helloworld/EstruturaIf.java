81b51261ab244e4e36042a6c86a16710bfad3ab6
FLAVIO SOARES DE SOUSA <a9261477@rn.senac.br>
2019-07-29 21:19:26 -0300

    primeira aula java

---
+++ src/br/senac/rn/helloworld/EstruturaIf.java
@@ -0,0 +1,15 @@
+package br.senac.rn.helloworld;
+
+public class EstruturaIf {
+    public static void main(String[] args) {
+        Double media = 7.5;
+
+        if (media >= 7) {
+            System.out.println("APROVADO");
+        } else if(media < 3){
+            System.out.println("REPROVADO");
+        } else {
+            System.out.println("RECUPERAÇÃO");
+        }
+    }
+}
---
+++ src/br/senac/rn/helloworld/HelloWorld.java
@@ -0,0 +1,38 @@
+package br.senac.rn.helloworld;
+
+import javax.swing.*;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+            //int valor1 = 2; //Tipo inteiro
+            // long idade = 32L; //Tipo inteiro
+            // short valor2 = 33; //Tipo inteiro
+
+            //float nota1 = 8.5f; //Tipo decimal
+            // double nota2 = 10.2;  //Tipo decimal
+
+            //boolean ehPrimo = true; //Tipo lógico
+
+            //char letra = 'd'; //Tipo literal (caractere)
+
+            //String frase = "Oi mundo"; //Tipo literal (texto)
+
+            Integer valor1 = 3;
+            Long valor2 = 2L;
+            Float nota1 = 8.5f;
+            Double nota2 = 7.5;
+            Boolean ehPrimo = true;
+            Character letra = 'd';
+            //Spring frase = "Oi mundo";
+
+        System.out.println(); //tecla de atalho sout
+
+
+
+
+
+            System.out.println("Bem vindo ao mundo Java!!");
+        }
+
+    }
